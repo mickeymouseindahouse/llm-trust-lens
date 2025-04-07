b3c64eb12f64a5060d4c424944db61dbea2a03f9
Michael Bernovskiy <46629985+mickeymouseindahouse@users.noreply.github.com>
2025-04-07 08:45:09 +0300

    add gitignore

--- llm_trust_lens/main.py
+++
@@ -1,5 +0,0 @@
-from llm_trust_lens.huggingface_embedding_model import HuggingFaceEmbeddingModel
-
-if __name__ == "__main__":
-    huggingface_embedding_model = HuggingFaceEmbeddingModel()
-    print(huggingface_embedding_model.retrieve_similar("This movie was scary and I didn't like it"))
